3ec843e1a2f649e180df0f53f71404ddf181ff1d
Heric Leite Rodrigues <hericleiterodrigues@gmail.com>
2025-09-29 20:21:18 -0300

    Feat: first commit

---
+++ CadastroDeUsuarios/src/test/java/com/rodrigues/heric/CadastroDeUsuarios/CadastroDeUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigues.heric.CadastroDeUsuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
